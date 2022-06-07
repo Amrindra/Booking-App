754873bf469621dcbdef1a917c1191410b399f0d
Amrindra Ouk <70451928+Amrindra@users.noreply.github.com>
2022-06-07 17:31:29 -0400

    added icons and styled header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,44 @@
 import "./Header.scss";
+import {
+  faBed,
+  faCar,
+  faPlane,
+  faTaxi
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="header_container">
+        <div className="header_lists">
+          <div className="header_list_item active">
+            <FontAwesomeIcon icon={faBed} />
+          </div>
+
+          <div className="header_list_item">
+            <FontAwesomeIcon icon={faPlane} />
+            <span>Flights</span>
+          </div>
+
+          <div className="header_list_item">
+            <FontAwesomeIcon icon={faCar} />
+            <span>Car rentals</span>
+          </div>
+
+          <div className="header_list_item">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Attractions</span>
+          </div>
+
+          <div className="header_list_item">
+            <FontAwesomeIcon icon={faTaxi} />
+            <span>Airport taxis</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
