baed70a0368ed0fecffffb8713c300f6622d1c91
Amrindra Ouk <70451928+Amrindra@users.noreply.github.com>
2022-06-08 18:04:55 -0400

    created propertyList component file

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -2,6 +2,7 @@ import "./Homepage.scss";
 import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
 import Featured from "../../components/featured/Featured";
+import PropertyList from "../../components/propertyList/PropertyList";
 
 const Homepage = () => {
   return (
@@ -11,6 +12,8 @@ const Homepage = () => {
 
       <section className="home_container">
         <Featured />
+        <h1 className="homepage_title">Brower by property type</h1>
+        <PropertyList />
       </section>
     </>
   );
