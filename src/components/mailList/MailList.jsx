e1dfefaca5aa1033508befec7e4a1d59fc604132
Amrindra Ouk <70451928+Amrindra@users.noreply.github.com>
2022-06-09 17:15:32 -0400

    Implemented subscribe component

--- src/components/mailList/MailList.jsx
+++
@@ -1,7 +0,0 @@
-import "./MailList.scss";
-
-const MailList = () => {
-  return <div></div>;
-};
-
-export default MailList;
--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -4,6 +4,7 @@ import Header from "../../components/header/Header";
 import HeroBanner from "../../components/heroBanner/HeroBanner";
 import PropertyList from "../../components/propertyList/PropertyList";
 import FeatureHotel from "../../components/featureHotel/FeatureHotel";
+import Subcribe from "../../components/subcribe/Subcribe";
 
 const Homepage = () => {
   return (
@@ -17,6 +18,7 @@ const Homepage = () => {
         <PropertyList />
         <h1 className="homepage_title">Homes gests love</h1>
         <FeatureHotel />
+        <Subcribe />
       </section>
     </>
   );
